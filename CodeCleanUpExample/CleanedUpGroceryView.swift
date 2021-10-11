652377bbf26c6fdf9d0acf35d4f8140ed601d2d7
Tom Phillips <tphillips21@msu.idserve.net>
2021-10-11 15:51:42 -0400

    Added comments + documentation links

--- CodeCleanUpExample/CleanedUpGroceryView.swift
+++ CodeCleanUpExample/CleanedUpGroceryView.swift
@@ -8,8 +8,13 @@
 import SwiftUI
 
 struct CleanedUpGroceryView: View {
+    // Storing the grocery items in an array allows us to loop through the same code instead of copy and pasting the same 10 lines over and over
     let groceryListItems = ["Pizza Crust", "Rotten Apples", "Spoiled Milk", "Gummy Rats", "Raw Meat Smoothie", "Schweaty Meatballs", "Shrimp", "Mayonaise Milkshake", "Chocolate"]
+    
+    // It is a good practice to store string values as constants so you can use it multiple places without risking a spelling mistake
     let imageName = "Oscarthegrouchy"
+    
+    // if you need to use a special character like quotations in a literal string, use a backslash. See 'Special Characters in String Literals' https://docs.swift.org/swift-book/LanguageGuide/StringsAndCharacters.html
     let navigationTitle = "Oscar's \"Gross\"eries"
     
     var body: some View {
@@ -18,6 +23,8 @@ struct CleanedUpGroceryView: View {
                 Image(imageName)
                     .resizable()
                     .scaledToFill()
+                
+                // Since the grocery list items are stored in an array, we can plug the array into a List to iterate through them and plug each item into the GroceryListItemView
                 List(groceryListItems, id: \.self) { groceryListItem in
                     GroceryListItemView(listItemText: groceryListItem)
                 }
@@ -26,6 +33,8 @@ struct CleanedUpGroceryView: View {
         }
     }
     
+    
+    // The init is a special function that is called whenever this View is created. This init is setting a couple UI colors.
     init() {
         UINavigationBar.appearance().backgroundColor = .orange
         UINavigationBar.appearance().largeTitleTextAttributes = [.foregroundColor: UIColor.white]
--- CodeCleanUpExample/GroceryListItemView.swift
+++ CodeCleanUpExample/GroceryListItemView.swift
@@ -7,8 +7,12 @@
 
 import SwiftUI
 
+// Breaking Views into small reusable pieces is crucial for keeping codebases clean
 struct GroceryListItemView: View {
+    // This value will store whether the checkbox has been selected for each individual instance of this View. See https://developer.apple.com/documentation/swiftui/state
     @State private var isSelected = false
+    
+    // Since this variable does not have a default value, Swift will force us to give a value for this when creating this View
     var listItemText: String
     
     var body: some View {
@@ -27,7 +31,10 @@ struct GroceryListItemView: View {
         .foregroundColor(.white)
     }
     
+    // This is an example of a 'Computed Property'. Rather than store a specific value, it runs code and returns the result. See Computed Properties https://docs.swift.org/swift-book/LanguageGuide/Properties.html
     var imageName: String {
+        
+        // This is an example of the Ternary Operator. The syntax looks very cryptic, but this is a handy shorthand that is the same as an if / else statement. See Ternary Conditional Operator https://docs.swift.org/swift-book/LanguageGuide/BasicOperators.html
         isSelected ? "checkmark.square" : "square"
     }
 }
