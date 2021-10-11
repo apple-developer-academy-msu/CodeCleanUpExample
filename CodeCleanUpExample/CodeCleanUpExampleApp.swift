384c0dbf94854d93b30f286ba591aa81059746a5
Tom Phillips <tphillips21@msu.idserve.net>
2021-10-11 15:05:14 -0400

    Initial Commit

---
+++ CodeCleanUpExample/CodeCleanUpExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodeCleanUpExampleApp.swift
+//  CodeCleanUpExample
+//
+//  Created by Tom Phillips on 10/11/21.
+//
+
+import SwiftUI
+
+@main
+struct CodeCleanUpExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
